162fc7b6455f25e7018cbf4ccfbe26745af43620
Anonymous <anonymous@example.com>
2025-08-28 14:54:24 +0300

    Update assembly info, runtime identifiers, and add sourcelink support

--- obj/Release/net8.0/WizdomSubsDownloader.AssemblyInfo.cs
+++ obj/Release/net8.0/WizdomSubsDownloader.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WizdomSubsDownloader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ac267bfa1a190c830d9231bccee7ffe6e57fa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WizdomSubsDownloader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WizdomSubsDownloader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
